5836f4a35590252d763d8631233a86a82e8aec3e
Blaine Price <1wbprice@gmail.com>
2016-07-26 11:32:18 -0400

    Add Trails boilerplate, set up Auth controller and service.

---
+++ api/services/AuthService.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Service = require('trails-service')
+
+/**
+ * @module AuthService
+ * @description Service for handling authentication requests
+ */
+module.exports = class AuthService extends Service {
+
+}
+
