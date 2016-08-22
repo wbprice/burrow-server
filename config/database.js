0b9b58bbfd659bad087a1ed349fd933bc69ed04f
Blaine Price <1wbprice@gmail.com>
2016-08-22 16:56:44 -0400

    Set up env vars and postgress connection. Include pg package.

--- config/database.js
+++ config/database.js
@@ -6,6 +6,17 @@
  *
  * @see {@link http://trailsjs.io/doc/config/database}
  */
+
+const dbName = process.env.PG_DATABASE_NAME || 'burrow'
+const dbUser = process.env.PG_DATABASE_USER || 'blaine'
+const dbPass = process.env.PG_DATABASE_PASS || ''
+const dbHost = process.env.PG_DATABASE_HOST || 'localhost'
+
+console.log('dbName: ', dbName)
+console.log('dbUser: ', dbUser)
+console.log('dbPass: ', dbPass)
+console.log('dbHost: ', dbHost)
+
 module.exports = {
 
   /**
@@ -15,23 +26,25 @@ module.exports = {
    *
    * Set production connection info in config/env/production.js
    */
+
   stores: {
 
-    /**
-     * Define a store called "local" which uses SQLite3 to persist data.
-     */
-
-    sqlitedev: {
-      database: 'dev',
-      storage: './.tmp/dev.sqlite',
-      host: '127.0.0.1',
-      dialect: 'sqlite'
+    postgres: {
+      dialect: 'postgres',
+      database: dbName,
+      host: dbHost,
+      username: dbUser,
+      password: dbPass,
+      port: 5432,
+      ssl: false,
+      migrate: 'alter'
     }
 
   },
 
   models: {
-    defaultStore: 'sqlitedev',
+    defaultStore: 'postgres',
     migrate: 'alter'
   }
+
 }
